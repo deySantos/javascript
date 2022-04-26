8a484e798b5f1c187cae5a5c6f430749118e604d
deySantos <deyssilvasantos4@gmail.com>
2022-04-26 11:25:50 -0300

    aula13 curso em vídeo

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+//ESTRUTURA DE REPETIÇÃO COM TESTE NO FINAL:
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+
+/*
+ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO INICIO :
+var c = 1
+while ( c <= 6 ) {
+    console.log('tudo bem!')
+    c++ // c = c + 1 
+}
+*/
+
+
+
+
+
+/*
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+console.log('tudo bem!')
+*/
