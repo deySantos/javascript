2dec04c0fb756ce886b9e81a4cb24a5be0b1c51b
deySantos <deyssilvasantos4@gmail.com>
2022-04-26 13:56:01 -0300

    Criei o exercício 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,16 @@
+console.log('Vai começar')
+ for(var c = 1; c <= 6; c++){
+     console.log(c)
+ }
+console.log('FIM!')
+
+
+
+
+
+/*
+ESTRUTURA DE REPETIÇÃO COM VARIAVEL DE CONTROLE
+for( var c =1; c<=6; c++) {
+    console.log(c)
+}
+*/
