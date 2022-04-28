c2daef13976249cf735690158602f25bbfbb08dd
deySantos <deyssilvasantos4@gmail.com>
2022-04-28 11:51:01 -0300

    Criei e atualizei ex 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,5 +1,29 @@
-function Contar(){
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+function Contar() {
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.getElementById('txtf')
+    var pas = window.document.getElementById('txtp')
+    var res = window.document.getElementById('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('[erro] Falta Dados!')
+    } else {
+        res.innerHTML ='Contando... <br>' 
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+
+        if ( p <= 0 ) {
+            window.alert('Acesso Invalido! Considerando [PASSO 1]')
+            p = 1
+        }
+        if ( 1 < f ) {
+            for( var c = i; c <= f; c += p)
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        else {
+            for( var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
