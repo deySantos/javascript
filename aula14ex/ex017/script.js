528385cec03877ae4a88d0dc92eac6b24ad333f1
deySantos <deyssilvasantos4@gmail.com>
2022-05-03 11:03:28 -0300

    criei e atualizei exercícios

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,20 +1,17 @@
-function tabuada() {
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
+function Tabuada() {
+    var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por Favor, Digite um número!')
     } else {
-        let n = Number(num.value)
-        let c = 1
-        tab.innerHTML = ''
-        while (c <= 10) {
-            let item = document.createElement('option')
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ``
+        while( c <= 10) {
+            var item = window.document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
-        }
-    }
-
-
+        }      }
 }
